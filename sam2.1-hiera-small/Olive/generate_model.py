692ec6208e1c4edad9d7b1ee6ee825a8c6c459c4
Shiva Chilukamari <chilukam+QCOM@qti.qualcomm.com>
2025-10-23 15:34:03 +0530

    Lintrunner changes done

--- sam2.1-hiera-small/Olive/generate_model.py
+++ sam2.1-hiera-small/Olive/generate_model.py
@@ -1,38 +1,33 @@
-import torch
-from torch import nn
-import torch.nn.functional as F
-from sam2.modeling.backbones.hieradet import MLP as SAM2MaskDecoderMLP
-from sam2.modeling.backbones.hieradet import (
-    MultiScaleBlock as SAMEncoderAttentionBlock,
-)
-from sam2.modeling.backbones.hieradet import do_pool
-from sam2.build_sam import build_sam2
-from hydra import initialize
-from hydra.core.global_hydra import GlobalHydra
-
-import onnx
-from onnxsim import simplify
+# -------------------------------------------------------------------------
+# Copyright (c) Microsoft Corporation. All rights reserved.
+# Licensed under the MIT License.
+# --------------------------------------------------------------------------
 
 import os
+
+import onnx
 import requests
+import torch
+import torch.nn.functional as F
+from hydra import initialize
+from hydra.core.global_hydra import GlobalHydra
+from onnxsim import simplify
+from sam2.build_sam import build_sam2
+from sam2.modeling.backbones.hieradet import do_pool
+from torch import nn
+
 
 def download_file(url, filename):
-    if os.path.exists(filename):
-        print(f"File '{filename}' already exists. Skipping download.")
-    else:
-        print(f"Downloading '{filename}' from {url}...")
+    if not os.path.exists(filename):
         response = requests.get(url, stream=True)
         if response.status_code == 200:
-            with open(filename, 'wb') as f:
+            with open(filename, "wb") as f:
                 for chunk in response.iter_content(chunk_size=8192):
                     f.write(chunk)
-            print(f"Download complete: '{filename}'")
-        else:
-            print(f"Failed to download file. Status code: {response.status_code}")
+
 
 class Conv2DInplaceLinear(nn.Module):
-    """
-    An implementation of Linear / Conv1D that uses a 1x1 Conv2D op instead.
+    """An implementation of Linear / Conv1D that uses a 1x1 Conv2D op instead.
 
     The Conv2D implementation for Qualcomm DSPs is faster than the Linear/Conv1D implementation.
     """
@@ -47,7 +42,7 @@ class Conv2DInplaceLinear(nn.Module):
         elif isinstance(mod, torch.nn.Conv1d):
             weight, bias = mod.weight.T, mod.bias
         else:
-            raise NotImplementedError()
+            raise NotImplementedError
 
         out_features, in_features = weight.shape
         linear = Conv2DInplaceLinear(
@@ -102,14 +97,15 @@ class Conv2DInplaceLinear(nn.Module):
             pass
         return x
 
+
 class SplitHeadSAMEncoderAttention(nn.Module):
-    """
-    SAM Attention block with the following modifications necessary to run on QNN:
-        * Heads are split into separate ops, rather than all heads running in a single op.
-        * QKV is unpacked from 1 tensor into 3 tensors.
+    """SAM Attention block with the following modifications necessary to run on QNN.
+
+    * Heads are split into separate ops, rather than all heads running in a single op.
+    * QKV is unpacked from 1 tensor into 3 tensors.
     """
 
-    def __init__(self, attention_block: SAMEncoderAttentionBlock) -> None:
+    def __init__(self, attention_block) -> None:
         super().__init__()
         self.out_feature, self.in_feature = (
             attention_block.qkv.weight.shape[0] // 3,
@@ -124,8 +120,8 @@ class SplitHeadSAMEncoderAttention(nn.Module):
         self.num_heads = attention_block.num_heads
         self.q_pool = attention_block.q_pool
 
-        for chunk, projList in enumerate([self.q, self.k, self.v]):
-            projList.conv2d.weight.data.copy_(
+        for chunk, proj_list in enumerate([self.q, self.k, self.v]):
+            proj_list.conv2d.weight.data.copy_(
                 attention_block.qkv.weight[
                     (chunk) * self.out_feature : (chunk + 1) * self.out_feature,
                     :,
@@ -134,51 +130,39 @@ class SplitHeadSAMEncoderAttention(nn.Module):
                 ]
             )
 
-            assert projList.conv2d.bias is not None
-            projList.conv2d.bias.data.copy_(
-                attention_block.qkv.bias[
-                    (chunk) * self.out_feature : (chunk + 1) * self.out_feature,
-                ]
+            assert proj_list.conv2d.bias is not None
+            proj_list.conv2d.bias.data.copy_(
+                attention_block.qkv.bias[(chunk) * self.out_feature : (chunk + 1) * self.out_feature,]
             )
 
     def forward(self, x: torch.Tensor) -> torch.Tensor:
-        B, H, W, _ = x.shape
+        b, h, w, _ = x.shape
         """
         #original code
-        # qkv with shape (B, H * W, 3, nHead, C)
-        qkv = self.qkv(x).reshape(B, H * W, 3, self.num_heads, -1)
-        # q, k, v with shape (B, H * W, nheads, C)
+        # qkv with shape (b, h * w, 3, nHead, C)
+        qkv = self.qkv(x).reshape(b, h * w, 3, self.num_heads, -1)
+        # q, k, v with shape (b, h * w, nheads, C)
         q, k, v = torch.unbind(qkv, 2)
         """
-        k = (
-            self.k(x)
-            .reshape(B, H * W, self.num_heads, -1)
-            .permute(0, 2, 1, 3)
-            .reshape(B * self.num_heads, H * W, -1)
-        )
-        v = (
-            self.v(x)
-            .reshape(B, H * W, self.num_heads, -1)
-            .permute(0, 2, 1, 3)
-            .reshape(B * self.num_heads, H * W, -1)
-        )
+        k = self.k(x).reshape(b, h * w, self.num_heads, -1).permute(0, 2, 1, 3).reshape(b * self.num_heads, h * w, -1)
+        v = self.v(x).reshape(b, h * w, self.num_heads, -1).permute(0, 2, 1, 3).reshape(b * self.num_heads, h * w, -1)
 
         # Q pooling (for downsample at stage changes)
         if self.q_pool:
             q = self.q(x)
             q = do_pool(q, self.q_pool)
-            H, W = q.shape[1:3]  # downsampled shape
-            q = q.reshape(B, H * W, self.num_heads, -1)
+            h, w = q.shape[1:3]  # downsampled shape
+            q = q.reshape(b, h * w, self.num_heads, -1)
             q = q.permute(0, 2, 1, 3)
-            q = q.reshape(B * self.num_heads, H * W, -1)
+            q = q.reshape(b * self.num_heads, h * w, -1)
         else:
             q = (
                 self.q(x)
-                .reshape(B, H * W, self.num_heads, -1)
+                .reshape(b, h * w, self.num_heads, -1)
                 .permute(0, 2, 1, 3)
-                .reshape(B * self.num_heads, H * W, -1)
+                .reshape(b * self.num_heads, h * w, -1)
             )
-        # Torch's SDPA expects [B, nheads, H*W, C] so we transpose
+        # Torch's SDPA expects [b, nheads, h*w, C] so we transpose
         x = F.scaled_dot_product_attention(
             q,
             k,
@@ -186,20 +170,17 @@ class SplitHeadSAMEncoderAttention(nn.Module):
         )
 
         # Transpose back
-        x = x.reshape(B, self.num_heads, H * W, -1)
+        x = x.reshape(b, self.num_heads, h * w, -1)
         x = x.transpose(1, 2)
-        x = x.reshape(B, H, W, -1)
+        x = x.reshape(b, h, w, -1)
 
-        x = self.proj(x)
+        return self.proj(x)
 
-        return x
 
 class Conv2DInplaceLinearSAMTransformerMLPBlock(nn.Module):
-    """
-    SAM MLPBlock that uses 1x1 Conv2D in place of linear layers.
-    """
+    """SAM MLPBlock that uses 1x1 Conv2D in place of linear layers."""
 
-    def __init__(self, mlp_block: SAM2MaskDecoderMLP) -> None:
+    def __init__(self, mlp_block) -> None:
         super().__init__()
         self.lin1 = Conv2DInplaceLinear.from_linear(mlp_block.layers[0])
         self.lin2 = Conv2DInplaceLinear.from_linear(mlp_block.layers[1])
@@ -208,51 +189,55 @@ class Conv2DInplaceLinearSAMTransformerMLPBlock(nn.Module):
     def forward(self, x: torch.Tensor) -> torch.Tensor:
         return self.lin2(self.act(self.lin1(x)))
 
-def window_partition(x, window_size):
-    """
-    Partition into non-overlapping windows with padding if needed.
-    Args:
-        x (tensor): input tokens with [B, H, W, C].
-        window_size (int): window size.
-    Returns:
-        windows: windows after partition with [B * num_windows, window_size, window_size, C].
-        (Hp, Wp): padded height and width before partition
-    """
-    B, H, W, C = x.shape
 
-    pad_h = (window_size - H % window_size) % window_size
-    pad_w = (window_size - W % window_size) % window_size
+def window_partition(x, window_size):
+    """Partition into non-overlapping windows with padding if needed.
+
+    Args:
+        x (tensor): input tokens with [b, h, w, c].
+        window_size (int): window size.
+
+    Returns:
+        windows: windows after partition with [b * num_windows, window_size, window_size, c].
+        (height_p, width_p): padded h and w before partition
+
+    """
+    b, h, w, c = x.shape
+
+    pad_h = (window_size - h % window_size) % window_size
+    pad_w = (window_size - w % window_size) % window_size
     if pad_h > 0 or pad_w > 0:
         x = F.pad(x, (0, 0, 0, pad_w, 0, pad_h))
-    Hp, Wp = H + pad_h, W + pad_w
+    height_p, width_p = h + pad_h, w + pad_w
 
-    x = x.view(Hp // window_size, window_size, Wp // window_size, window_size, C)
-    windows = x.permute(0, 2, 1, 3, 4).reshape(-1, window_size, window_size, C)
-    return windows, (Hp, Wp)
+    x = x.view(height_p // window_size, window_size, width_p // window_size, window_size, c)
+    windows = x.permute(0, 2, 1, 3, 4).reshape(-1, window_size, window_size, c)
+    return windows, (height_p, width_p)
 
 
-def window_unpartition(windows, window_size, pad_hw, hw):
-    """
-    Window unpartition into original sequences and removing padding.
+def window_unpartition(x, window_size, pad_hw, hw):
+    """Window unpartition into original sequences and removing padding.
+
     Args:
-        x (tensor): input tokens with [B * num_windows, window_size, window_size, C].
+        x (tensor): input tokens with [b * num_windows, window_size, window_size, c].
         window_size (int): window size.
-        pad_hw (Tuple): padded height and width (Hp, Wp).
-        hw (Tuple): original height and width (H, W) before padding.
-    Returns:
-        x: unpartitioned sequences with [B, H, W, C].
-    """
-    Hp, Wp = pad_hw
-    H, W = hw
-    B = windows.shape[0] // (Hp * Wp // window_size // window_size)
-    x = windows.reshape(
-        Hp // window_size, Wp // window_size, window_size, window_size, -1
-    )
-    x = x.permute(0, 2, 1, 3, 4).reshape(B, Hp, Wp, -1)
+        pad_hw (Tuple): padded h and w (height_p, width_p).
+        hw (Tuple): original h and w (h, w) before padding.
 
-    if Hp > H or Wp > W:
-        x = x[:, :H, :W, :]
+    Returns:
+        x: unpartitioned sequences with [b, h, w, c].
+
+    """
+    height_p, width_p = pad_hw
+    h, w = hw
+    b = x.shape[0] // (height_p * width_p // window_size // window_size)
+    x = x.reshape(height_p // window_size, width_p // window_size, window_size, window_size, -1)
+    x = x.permute(0, 2, 1, 3, 4).reshape(b, height_p, width_p, -1)
+
+    if height_p > h or width_p > w:
+        x = x[:, :h, :w, :]
     return x
+
 
 class ModMultiScaleBlock(nn.Module):
     def __init__(self, block):
@@ -262,7 +247,7 @@ class ModMultiScaleBlock(nn.Module):
         self.model.attn = SplitHeadSAMEncoderAttention(self.model.attn)
 
     def forward(self, x: torch.Tensor) -> torch.Tensor:
-        shortcut = x  # B, H, W, C
+        shortcut = x  # B, h, w, C
         x = self.model.norm1(x)
 
         # Skip connection
@@ -272,7 +257,7 @@ class ModMultiScaleBlock(nn.Module):
         # Window partition
         window_size = self.model.window_size
         if window_size > 0:
-            H, W = x.shape[1], x.shape[2]
+            h, w = x.shape[1], x.shape[2]
             x, pad_hw = window_partition(x, window_size)
 
         # Window Attention + Q Pooling (if stage change)
@@ -280,20 +265,20 @@ class ModMultiScaleBlock(nn.Module):
         if self.model.q_stride:
             # Shapes have changed due to Q pooling
             window_size = self.model.window_size // self.model.q_stride[0]
-            H, W = shortcut.shape[1:3]
+            h, w = shortcut.shape[1:3]
 
-            pad_h = (window_size - H % window_size) % window_size
-            pad_w = (window_size - W % window_size) % window_size
-            pad_hw = (H + pad_h, W + pad_w)
+            pad_h = (window_size - h % window_size) % window_size
+            pad_w = (window_size - w % window_size) % window_size
+            pad_hw = (h + pad_h, w + pad_w)
 
         # Reverse window partition
         if self.model.window_size > 0:
-            x = window_unpartition(x, window_size, pad_hw, (H, W))
+            x = window_unpartition(x, window_size, pad_hw, (h, w))
 
         x = shortcut + self.model.drop_path(x)
         # MLP
-        x = x + self.model.drop_path(self.model.mlp(self.model.norm2(x)))
-        return x
+        return x + self.model.drop_path(self.model.mlp(self.model.norm2(x)))
+
 
 class SAM2Encoder(nn.Module):
     """Exportable SAM2 encoder that can be split into several parts."""
@@ -304,17 +289,18 @@ class SAM2Encoder(nn.Module):
     ) -> None:
         super().__init__()
         self.model = sam2
-        self._bb_feat_sizes = [(256, 256), (128, 128), (64, 64),]
+        self._bb_feat_sizes = [
+            (256, 256),
+            (128, 128),
+            (64, 64),
+        ]
         for i, block in enumerate(self.model.image_encoder.trunk.blocks):
             self.model.image_encoder.trunk.blocks[i] = ModMultiScaleBlock(block)
 
-    def forward(
-        self, Image: torch.Tensor
-    ) -> tuple[torch.Tensor, torch.Tensor, torch.Tensor]:
-        """
-        Run SAM2 Image encoder and returns image_embeddings, high_res_features1, high_res_features2
+    def forward(self, Image: torch.Tensor) -> tuple[torch.Tensor, torch.Tensor, torch.Tensor]:
+        """Run SAM2 Image encoder and returns image_embeddings, high_res_features1, high_res_features2.
 
-        Parameters:
+        Args:
             Image:
                 Raw floating point pixel values for encoder consumption.
                 3-channel Color Space: RGB, range [0, 1]
@@ -323,6 +309,7 @@ class SAM2Encoder(nn.Module):
                 image_embeddings: Shape (1, 256, 64, 64)
                 high_res_features1: Shape (1, 32, 256, 256)
                 high_res_features2: Shape (1, 64, 128, 128)
+
         """
         # x = self.normalize(Image)
         x = Image
@@ -340,9 +327,9 @@ class SAM2Encoder(nn.Module):
         high_res_features2 = feats[1]
         return image_embeddings, high_res_features1, high_res_features2
 
+
 class SAM2Decoder(nn.Module):
-    """.
-    This SAM2Decoder is taken from the class SAM2ImagePredictor.predict from sam2.
+    """SAM2Decoder is taken from the class SAM2ImagePredictor.predict from sam2.
 
     This removes output mask resizing. Because this requires a dynamic shape to accomplish
     in the network, it's better to do this as a postprocessing step rather than in the inference
@@ -363,10 +350,9 @@ class SAM2Decoder(nn.Module):
         unnorm_coords: torch.Tensor,  # [num_labels,num_points,2]
         labels: torch.Tensor,  # [num_labels,num_points]
     ) -> tuple[torch.Tensor, torch.Tensor]:
-        """
-        Run SAM2 lightweight decoder and return generated mask for given points
+        """Run SAM2 lightweight decoder and return generated mask for given points.
 
-        Parameters:
+        Args:
             image_embeddings: torch.Tensor of shape [1, emb_dim, emb_size, emb_size]
                 Image embeddings generated by Encoder
             high_res_features1: torch.Tensor of shape [1, high_res_1_dim, high_res_1_size, high_res_1_size]
@@ -382,6 +368,7 @@ class SAM2Decoder(nn.Module):
         Returns:
             masks: torch.Tensor of shape [1, 1, 256, 256]
             scores: torch.Tensor of shape [1, 1]
+
         """
         sparse_embedding, dense_embedding = self.prompt_encoder(
             points=(unnorm_coords, labels),
@@ -398,10 +385,12 @@ class SAM2Decoder(nn.Module):
         )
         return low_res_masks, iou_predictions
 
+
 model_weights_url = "https://dl.fbaipublicfiles.com/segment_anything_2/092824/sam2.1_hiera_small.pt"
 model_config_url = "https://huggingface.co/facebook/sam2.1-hiera-small/resolve/main/sam2.1_hiera_s.yaml"
 checkpoint = "sam2.1_hiera_small.pt"
 model_cfg = "sam2.1_hiera_s.yaml"
+
 
 def main():
     download_file(model_weights_url, checkpoint)
@@ -409,36 +398,36 @@ def main():
 
     GlobalHydra.instance().clear()
     initialize(config_path="./", job_name="sam2_inference", version_base=None)
-    
+
     sam2_model = build_sam2(model_cfg, checkpoint, device="cpu")
     encoder = SAM2Encoder(sam2_model)
     decoder = SAM2Decoder(sam2_model)
-    en_inputs = {'Image': torch.rand((1, 3, 1024, 1024))}
-    de_inputs = {"image_embeddings": torch.rand([1, 256, 64, 64]), "high_res_features1":  torch.rand([1, 32, 256, 256]),
-             "high_res_features2": torch.rand((1, 64, 128, 128)), "unnorm_coords": torch.randn((1,5,2)), "labels": torch.ones((1, 5))}
+    en_inputs = {"Image": torch.rand((1, 3, 1024, 1024))}
+    de_inputs = {
+        "image_embeddings": torch.rand([1, 256, 64, 64]),
+        "high_res_features1": torch.rand([1, 32, 256, 256]),
+        "high_res_features2": torch.rand((1, 64, 128, 128)),
+        "unnorm_coords": torch.randn((1, 5, 2)),
+        "labels": torch.ones((1, 5)),
+    }
 
     with torch.no_grad():
-        torch.onnx.export(encoder, en_inputs, "sam21_vision_encoder.onnx", opset_version = 20, do_constant_folding = True)
+        torch.onnx.export(encoder, en_inputs, "sam21_vision_encoder.onnx", opset_version=20, do_constant_folding=True)
     with torch.no_grad():
-        torch.onnx.export(decoder, de_inputs, "sam21_mask_decoder.onnx", opset_version = 20, do_constant_folding = True)
+        torch.onnx.export(decoder, de_inputs, "sam21_mask_decoder.onnx", opset_version=20, do_constant_folding=True)
 
     encoder_onnx_model = onnx.load("sam21_vision_encoder.onnx")
     simplified_encoder_onnx_model, check = simplify(encoder_onnx_model)
-    
+
     if check:
-        onnx.save(simplified_encoder_onnx_model, "sam21_vision_encoder_sim.onnx")
-        print("Simplified encoder model saved to: sam21_vision_encoder_sim.onnx")
-    else:
-        print("Simplification of encoder model failed. Please check the model.")
+        onnx.save(simplified_encoder_onnx_model, "sam21_vision_encoder.onnx")
 
     decoder_onnx_model = onnx.load("sam21_mask_decoder.onnx")
     simplified_decoder_onnx_model, check = simplify(decoder_onnx_model)
-    
-    if check:
-        onnx.save(simplified_decoder_onnx_model, "sam21_mask_decoder_sim.onnx")
-        print("Simplified decoder model saved to: sam21_mask_decoder_sim.onnx")
-    else:
-        print("Simplification of decoder model failed. Please check the model.")
 
-if __name__ == '__main__':
+    if check:
+        onnx.save(simplified_decoder_onnx_model, "sam21_mask_decoder.onnx")
+
+
+if __name__ == "__main__":
     main()
