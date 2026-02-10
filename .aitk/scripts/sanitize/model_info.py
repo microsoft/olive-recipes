dd6f1060b283f5bd2e23e4dee6b2c99211b637f8
Shiyi Zheng (from Dev Box) <shzhen@microsoft.com>
2026-02-10 14:28:31 +0800

    fix typo

--- .aitk/scripts/sanitize/model_info.py
+++ .aitk/scripts/sanitize/model_info.py
@@ -21,7 +21,6 @@ from .utils import GlobalVars, open_ex, printError, printProcess
 
 class ModelInfo(BaseModel):
     displayName: str
-    discription: Optional[str] = None
     description: Optional[str] = None
     icon: IconEnum
     modelLink: str
