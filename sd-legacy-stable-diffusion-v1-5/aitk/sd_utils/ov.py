f09df5b02028bc4684aedfd680dccd4c70b980f7
ziyuanguo <ziyuanguo@microsoft.com>
2026-04-07 06:09:25 +0800

    fix

--- sd-legacy-stable-diffusion-v1-5/aitk/sd_utils/ov.py
+++ sd-legacy-stable-diffusion-v1-5/aitk/sd_utils/ov.py
@@ -146,21 +146,7 @@ class OVStableDiffusionPipeline(OnnxStableDiffusionPipeline):
         image = np.clip(image / 2 + 0.5, 0, 1)
         image = image.transpose((0, 2, 3, 1))
 
-        if self.safety_checker is not None:
-            safety_checker_input = self.feature_extractor(
-                self.numpy_to_pil(image), return_tensors="np"
-            ).pixel_values.astype(image.dtype)
-
-            images, has_nsfw_concept = [], []
-            for i in range(image.shape[0]):
-                image_i, has_nsfw_concept_i = self.safety_checker(
-                    clip_input=safety_checker_input[i : i + 1], images=image[i : i + 1]
-                )
-                images.append(image_i)
-                has_nsfw_concept.append(has_nsfw_concept_i[0])
-            image = np.concatenate(images)
-        else:
-            has_nsfw_concept = None
+        has_nsfw_concept = None
 
         if output_type == "pil":
             image = self.numpy_to_pil(image)
