c9af02136c36c3453be2649d1042f5d670a3afaf
Ur Rahman, Tanzeel <Tanzeel.UrRahman@amd.com>
2026-04-22 17:05:51 +0530

    Update Licence

--- google-translategemma-4b-it/builtin/optimize.py
+++ google-translategemma-4b-it/builtin/optimize.py
@@ -1,3 +1,7 @@
+# -------------------------------------------------------------------------
+# Copyright (C) 2026 Advanced Micro Devices, Inc. All rights reserved.
+# Portions of this file consist of AI generated content.
+# --------------------------------------------------------------------------
 """End-to-end optimization pipeline for TranslateGemma-4B-IT VLM.
 
 Exports three sub-models (text decoder, vision encoder, embedding),
