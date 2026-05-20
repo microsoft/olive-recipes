6281c0a3b01da4a32b4596b663ede931fd62bf17
hualxie <hualxie@microsoft.com>
2026-05-20 15:59:32 +0800

    nit

--- openai-whisper-large-v3-turbo/aitk/ov_evaluate.py
+++ openai-whisper-large-v3-turbo/aitk/ov_evaluate.py
@@ -82,7 +82,7 @@ def main():
     resultStr = json.dumps(metrics, indent=4)
     with open(args.output_file, 'w') as file:
         file.write(resultStr)
-    print("Model lab succeeded for evaluation.\n%s", resultStr)
+    print("Model lab succeeded for evaluation.\n", resultStr)
 
 
 if __name__ == "__main__":
--- openai-whisper-large-v3-turbo/aitk/qnn_evaluate.py
+++ openai-whisper-large-v3-turbo/aitk/qnn_evaluate.py
@@ -85,7 +85,7 @@ def main():
     resultStr = json.dumps(metrics, indent=4)
     with open(args.output_file, 'w') as file:
         file.write(resultStr)
-    logger.info("Model lab succeeded for evaluation.\n%s", resultStr)
+    logger.info("Model lab succeeded for evaluation.\n", resultStr)
 
 
 if __name__ == "__main__":
